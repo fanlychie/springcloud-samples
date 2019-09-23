5b0ba99d9cf631facdc87ad07499a1ff77cd5421
fanlychie <fanlychie@yeah.net>
2019-09-23 18:08:57 +0800

    add hystrix

---
+++ springcloud-hystrix/src/main/java/org/fanlychie/sample/HystrixApplication.java
@@ -0,0 +1,17 @@
+package org.fanlychie.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author fanlychie
+ * @date 2019/09/18
+ */
+@SpringBootApplication
+public class HystrixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixApplication.class, args);
+    }
+
+}
