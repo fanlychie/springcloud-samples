b32fecfc299246d38aeb8a6c89d1aa646b2ff688
fanlychie <fanlychie@yeah.net>
2019-09-26 11:14:48 +0800

    add random sleep time

--- springcloud-eureka-client-provider/src/main/java/org/fanlychie/sample/controller/MessageController.java
+++ springcloud-eureka-client-provider/src/main/java/org/fanlychie/sample/controller/MessageController.java
@@ -5,6 +5,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
+
 /**
  * @author fanlychie
  * @date 2019/9/20
@@ -20,8 +23,9 @@ public class MessageController {
     }
 
     @GetMapping("/sayHi")
-    public String sayHi() {
+    public String sayHi() throws InterruptedException {
         log.info("======== HI ========");
+        TimeUnit.SECONDS.sleep(ThreadLocalRandom.current().nextInt(2));
         return "Hi, my friend.";
     }
 
