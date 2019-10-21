3e6c5234b96341f2c7ac6307c3d842070656bd29
fanlychie <fanlychie@yeah.net>
2019-10-21 18:15:40 +0800

    add zuul

---
+++ springcloud-zuul/src/main/java/org/fanlychie/sample/ZuulApplication.java
@@ -0,0 +1,10 @@
+package org.fanlychie.sample;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class ZuulApplication {
+
+}
